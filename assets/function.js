4f352afa478b389e2e220b16ed1372527834b617
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2021-05-15 13:33:56 +0700

    Create function.js

---
+++ assets/function.js
@@ -0,0 +1,9 @@
+var canvasDiv = document.getElementById('container');
+var options = {
+  particleColor: '#fff',
+  background: 'https://images.pexels.com/photos/2261017/pexels-photo-2261017.jpeg',
+  interactive: true,
+  speed: 'high',
+  density: 'high'
+};
+var particleCanvas = new ParticleNetwork(canvasDiv, options);
