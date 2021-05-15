bf77384728464ccf62997925a301d226fbc50d09
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2021-05-15 13:57:10 +0700

    Update function.js

--- assets/function.js
+++ assets/function.js
@@ -1,6 +1,7 @@
-var canvasDiv = document.getElementById('container');
+var canvasDiv = document.getElementById('canvas');
 var options = {
   particleColor: '#fff',
+  background: 'https://images.pexels.com/photos/2261017/pexels-photo-2261017.jpeg',
   interactive: true,
   speed: 'high',
   density: 'high'
