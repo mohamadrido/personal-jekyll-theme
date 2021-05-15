ed09d2087dfd3d7a80f8704290e7967b3def9605
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2021-05-15 14:02:50 +0700

    Delete function.js

--- assets/function.js
+++
@@ -1,9 +0,0 @@
-var canvasDiv = document.getElementById('canvas');
-var options = {
-  particleColor: '#fff',
-  background: 'https://images.pexels.com/photos/2261017/pexels-photo-2261017.jpeg',
-  interactive: true,
-  speed: 'high',
-  density: 'high'
-};
-var particleCanvas = new ParticleNetwork(canvasDiv, options);
